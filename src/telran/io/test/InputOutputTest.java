6960275d04bb3f112bc9954109bf8bc5e5fdf6a3
kirill2884 <kirill2884@gmail.com>
2023-03-05 15:42:48 +0200

    HW 36 printDirectory

--- src/telran/io/test/InputOutputTest.java
+++ src/telran/io/test/InputOutputTest.java
@@ -4,20 +4,143 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class InputOutputTest {
-
+	
+	private static String OFFSET = "  ";
+	private static String OFFSET_DIR_NAME = "";
+	
+	String fileName = "newFile";
+	String directoryName = "newDirectory1/newDirectory2";
 	@BeforeEach
 	void setUp() throws Exception {
+		new File(fileName).delete();
+		new File(directoryName).delete();
 	}
 
 	@Test
-	void test() throws IOException {
-		File file1 = new File("newFile");
+	@Disabled
+	void testFile() throws IOException {
+		File file1 = new File("newDirectory1/" + fileName + "1");
 		assertTrue(file1.createNewFile());
+		File directory = new File(directoryName);
+		directory.mkdirs();
+		assertTrue(directory.mkdirs());
+		File file2 = new File(directoryName + "/" + fileName + "2");
+		file2.createNewFile();
+		System.out.println(directory.getAbsolutePath());
+	}
+	
+	@Test
+	@Disabled
+	void testFiles() {
+		Path path = Path.of(".");
+		System.out.println(path.toAbsolutePath().getNameCount());	
+	}
+	
+	@Test
+	void printDirectoryTest() throws IOException {
+		printDirectoryFile(".", 0);
+		printDirectoryFiles(".", 0);
+	}
+	
+	//---------------------- class File --------------------------------------
+	void printDirectoryFile(String pathParam, int maxLevel) {
+		Path path = Path.of(pathParam);
+		System.out.println("-------- used class 'File' ---------");
+		
+		if (pathParam.equals(".")) {
+			System.out.println(OFFSET_DIR_NAME + path.toAbsolutePath().getParent().getFileName() + "|dir");
+		} else {
+			System.out.println(OFFSET_DIR_NAME + path.getFileName() + "|dir");
+		}
+		
+		if (maxLevel < 1) {
+			allPathPrinting(pathParam, OFFSET);
+		} else {
+			levelPathPrinting(pathParam, maxLevel, OFFSET);
+		}		
 	}
 
+	private void levelPathPrinting(String pathParam, int maxLevel, String offset) {
+		File file = new File(pathParam);
+		File[] arrFile = file.listFiles();
+		if (maxLevel > 0) {
+			for (File fl : arrFile) {
+				if (fl.isDirectory()) {
+					System.out.println(offset + fl.getName() + "| dir");
+					String newPath = pathParam + "/" + fl.getName();
+					levelPathPrinting(newPath, --maxLevel, offset + "   " );
+				} else {
+					System.out.println(offset + fl.getName() + "| file");
+				}
+			}		
+		}
+	}
+
+	private void allPathPrinting(String pathParam, String offset) {
+		File file = new File(pathParam);
+		File[] arrFile = file.listFiles();
+		for (File fl : arrFile) {
+			if (fl.isDirectory()) {
+				System.out.println(offset + fl.getName() + "| dir");
+				String newPath = pathParam + "/" + fl.getName();
+				allPathPrinting(newPath, offset + "   " );
+			} else {
+				System.out.println(offset + fl.getName() + "| file");
+			}
+		}
+	}
+	
+	
+	//------------------------- class Files--------------------------------------
+	void printDirectoryFiles(String pathParam, int maxLevel) throws IOException {
+		Path path = Path.of(pathParam);
+		System.out.println("-------- used class 'Files' ---------");
+		if (pathParam.equals(".")) {
+			System.out.println(OFFSET_DIR_NAME + path.toAbsolutePath().getParent().getFileName() + "|dir");
+		} else {
+			System.out.println(OFFSET_DIR_NAME + path.getFileName() + "|dir");
+		}
+		
+		if (maxLevel < 1) {
+			printAllPathFiles(path);
+		} else {
+			printLevelPathFiles(maxLevel, path);
+		}
+	
+	}
+
+	private void printLevelPathFiles(int maxLevel, Path path) throws IOException {
+		Files.walk(path, maxLevel, FileVisitOption.FOLLOW_LINKS)
+		.filter(n -> n != path)
+		.forEach(n -> {
+			if (Files.isDirectory(n)) {
+				System.out.println(" ".repeat(n.getNameCount()) +  n.getFileName() + "| dir");
+			} else {
+				System.out.println(" ".repeat(n.getNameCount()) +  n.getFileName() + "|file");
+			}
+			
+		});
+	}
+
+	private void printAllPathFiles(Path path) throws IOException {
+		Files.walk(path, FileVisitOption.FOLLOW_LINKS)
+		.filter(n -> n != path)
+		.forEach(n -> {
+			if (Files.isDirectory(n)) {
+				System.out.println(" ".repeat(n.getNameCount()) +  n.getFileName() + "| dir");
+			} else {
+				System.out.println(" ".repeat(n.getNameCount()) +  n.getFileName() + "|file");
+			}
+			
+		});
+	}
+	
 }
